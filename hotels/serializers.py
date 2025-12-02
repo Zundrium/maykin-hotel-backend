e44717675a5ca1696c0f20408581c263fc8a7147
Sem Verbraak <Zundrium@users.noreply.github.com>
2025-12-02 14:05:18 +0100

    added 'id' field to the viewsets

--- hotels/serializers.py
+++ hotels/serializers.py
@@ -4,11 +4,11 @@ from .models import City, Hotel
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
-        fields = ['code', 'name']
+        fields = ['id','code', 'name']
 
 class HotelSerializer(serializers.ModelSerializer):
     city_name = serializers.CharField(source='city.name', read_only=True)
 
     class Meta:
         model = Hotel
-        fields = ['city', 'city_name', 'zone', 'name']
+        fields = ['id', 'city', 'city_name', 'zone', 'name']
